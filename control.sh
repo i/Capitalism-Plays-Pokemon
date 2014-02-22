1aca78cb6cd42ec7314b319e8a9becd133665928
Jesse Huang <jh3141@gmail.com>
2014-02-22 17:08:22 -0500

    updated control

--- control.sh
+++ control.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-WID=`xdotool search --name "VBA-M" | head -1`
+
+if [ "$(whoami)" == 'jhuang' ]; then
+    WID=`xdotool search --name "VBA-M" | head -1`
+fi
+if [ "$(whoami)" == 'ian' ]; then
+    WID=`xdotool search --name "VisualBoyAdvance" | head -1`
+fi
+
 xdotool windowactivate $WID
+sleep 0.1
 xdotool keydown Down
 sleep 0.125
 xdotool keyup Down
