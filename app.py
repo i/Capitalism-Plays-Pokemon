9ba376cd26b02215e7cc7fe4c99a38352e9db25d
Ian Lozinski <ian.lozinski@gmail.com>
2014-02-22 18:52:19 -0500

    Flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-def pressButton(button):
-  os.exec('xdotool windowactivate $wid')
-  os.exec('xdotool windowactivate $WID')
-  os.exec('sleep 0.1')
-  os.exec('xdotool keydown ' + button)
-  os.exec('sleep 0.125')
-  os.exec('xdotool keyup ' + button)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+
+if __name__ == "__main__":
+  app.run()
