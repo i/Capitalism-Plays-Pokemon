d84c6bcc78d4565fb785a0f5970634b99a6fe1a5
Jesse Huang <jh3141@gmail.com>
2014-02-23 03:35:43 -0500

    updated sendgrid post

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'cgi'
+require 'pp'
 
 # Used when setting URL on Venmo
 # https://venmo.com/account/settings/developers
@@ -17,10 +19,22 @@ post '/' do
   note = @json['data']['note'].downcase
   puts note
   `./control.sh #{note}`
-
+  
   200
 end
 
+post '/email' do
+  @subject = ""
+  @text = ""
+  request.POST.each do |k,v| 
+    @subject = v if k == "subject" 
+    @text = v if k == "text"
+  end
+  puts @subject
+  `./control.sh #{@subject}`
+  
+  200
+end
 
 
 
